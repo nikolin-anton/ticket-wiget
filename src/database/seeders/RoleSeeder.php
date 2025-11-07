97ab80ee84a7bbbb71a40b15af8c9b8f31cb5106
Anton Nikolin <antonnikolin@gmail.com>
2025-11-07 13:20:31 +0200

    created model user and customer and added users permissions

---
+++ src/database/seeders/RoleSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $role_admin = Role::create(['name' => 'admin']);
+        $permissions_admin = Permission::create(['name' => 'full_manage_tickets']);
+        $role_admin->givePermissionTo($permissions_admin);
+
+        $role_manager = Role::create(['name' => 'manager']);
+        $permissions_manager = Permission::create(['name' => 'edit_tickets']);
+        $role_manager->givePermissionTo($permissions_manager);
+
+        User::factory()->create([
+            'name' => 'admin',
+            'email' => 'admin@mail.com',
+            'password' => '12345678'
+        ])->assignRole($role_admin);
+
+        User::factory()->create([
+            'name' => 'manager',
+            'email' => 'manager@mail.com',
+            'password' => '12345678'
+        ])->assignRole($role_manager);
+    }
+}
